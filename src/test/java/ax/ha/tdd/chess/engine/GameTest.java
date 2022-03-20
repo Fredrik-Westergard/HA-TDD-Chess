a11b1500e678ef60203ca1debff2b4911ee811a1
fredrik Westergård <fredrik.westergard@ha.ax>
2022-03-20 19:28:47 +0200

    Added test for invalid rook moves

--- src/test/java/ax/ha/tdd/chess/engine/GameTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/GameTest.java
@@ -132,4 +132,38 @@ public class GameTest {
         assertEquals(game.getLastMoveResult(), "Last move was successful");
         assertEquals(game.getPlayerToMove(), Player.BLACK);
     }
+
+    @Test
+    public void testIsMoveRookInvalid(){
+        Game game = new Game();
+        assertEquals(game.getPlayerToMove(), Player.WHITE);
+        String str = "a2-a4";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was successful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+        str = "b7-b5";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was successful");
+        assertEquals(game.getPlayerToMove(), Player.WHITE);
+        str = "a4-b5";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was successful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+        str = "a8-b3";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was unsuccessful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+        str = "a8-b8";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was unsuccessful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+        str = "a8-b3";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was unsuccessful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+        str = "a8-h1";
+        game.move(str);
+        assertEquals(game.getLastMoveResult(), "Last move was unsuccessful");
+        assertEquals(game.getPlayerToMove(), Player.BLACK);
+    }
 }
